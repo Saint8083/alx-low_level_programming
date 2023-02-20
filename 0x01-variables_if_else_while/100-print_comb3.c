cf2b66f22002c8991e979e8ec3fecb919ca90113
saint8083 <kvictoria040@gmail.com>
2023-02-19 18:06:45 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -17,6 +18,10 @@ int main(void)
 	{
 	if (p != d)
 	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && '9')
+	continue;
 	putchar(',');
 	putchar(' ');
 	}
