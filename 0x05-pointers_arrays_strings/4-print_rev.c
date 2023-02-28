ed76213d3442466ef7e1dcabd8d1071bd8b665da
saint8083 <kvictoria040@gmail.com>
2023-02-28 15:05:28 -0800

    My C - Pointers, arrays and strings tasks

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,23 @@
-include "main.h"
+#include "main.h"
+#include <stdio.h>
+
 /**
  * print_rev - print a sring in reverse
  * @s: string to be printed
+ * Return: void
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
+	{
+	i++;
+	}
 
+	for (i -= 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
 	_putchar('\n');
 }
