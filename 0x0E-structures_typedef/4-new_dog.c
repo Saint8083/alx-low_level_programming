0efbf32b89ce71898731cc018157d27a54c5f3a5
saint8083 <kvictoria040@gmail.com>
2023-03-22 06:01:19 -0700

    my poppy flls

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,7 +43,7 @@ char *_strcpy(char *dest, char *src)
 	len = 0;
 
 
-while (src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
@@ -69,37 +69,37 @@ while (src[len] != '\0')
  * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
-	{
-		dog_t *dog;
-		int len1, len2;
+{
+	dog_t *dog;
+	int len1, len2;
 
 
-		len1 = _strlen(name);
-		len2 = _strlen(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 
-		dog = malloc(sizeof(dog_t));
-		if (dog == NULL)
-			return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	return (NULL);
 
 
-		dog->name = malloc(sizeof(char) * (len1 + 1));
-		if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+{
+	free(dog);
+	return (NULL);
+}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
-	{
-		free(dog);
-		free(dog->name);
-		return (NULL);
-	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
+{
+	free(dog);
+	free(dog->name);
+	return (NULL);
+}
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
 
 
-	return (dog);
+return (dog);
 }
