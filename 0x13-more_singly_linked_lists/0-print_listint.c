e079748efa8c8aa912a8446918a10582ae919958
saint8083 <kvictoria040@gmail.com>
2023-04-04 18:14:05 -0700

    My C project teask

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,6 @@
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
- *
  */
 size_t print_listint(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,6 +7,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head == NULL)
 	return;
 
@@ -16,5 +17,6 @@ void free_listint2(listint_t **head)
 	free(*head);
 	*head = temp;
 	}
+
 	*head = NULL;
 }
